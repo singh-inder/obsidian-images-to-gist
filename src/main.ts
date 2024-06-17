bef373487775c9357f2b5f705e87d15f698269fd
Inder <85822513+inderrr@users.noreply.github.com>
2024-06-17 17:56:36 +0530

    feat(main.ts): load env vars
    - feat(main.ts): loadEnvVars method to setup dotenv and parse env vars.
    - getToken method will check if there is GITHUB_TOKEN present inside envs or in settings(temporary), and if there isn't show a Notice.

--- src/main.ts
+++ src/main.ts
@@ -1,97 +1,67 @@
-import {
-	App,
-	Editor,
-	MarkdownView,
-	Modal,
-	Notice,
-	Plugin,
-	PluginSettingTab,
-	Setting
-} from "obsidian";
+import { FileSystemAdapter, Notice, Plugin, normalizePath } from "obsidian";
+import * as dotenv from "dotenv";
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
-}
-
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: "default"
+type ImagesFromGistSettings = {
+	showConfirmationModal: boolean;
+	githubToken?: string;
+	serverUrl: string;
 };
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+const DEFAULT_SETTINGS: ImagesFromGistSettings = {
+	// TODO: update default server url to be deployed server domain
+	serverUrl: "http://localhost:5000",
+	showConfirmationModal: true
+};
+
+export default class ImagesFromGist extends Plugin {
+	pluginName = "images-from-gist";
+
+	settings: ImagesFromGistSettings;
+
+	private noGithubTokenNotice(addToStatusBar: boolean = false) {
+		const errText = "❌ No Github token";
+		new Notice(errText, 3 * 1000);
+
+		if (addToStatusBar) this.addStatusBarItem().setText(errText);
+	}
+
+	// https://forum.obsidian.md/t/how-to-get-current-plugins-directory/26427/2
+	getAbsolutePath(fileName: string): string {
+		let basePath;
+
+		if (this.app.vault.adapter instanceof FileSystemAdapter) {
+			basePath = this.app.vault.adapter.getBasePath();
+		} else {
+			throw new Error("Cannot determine base path.");
+		}
+
+		const relativePath = `${this.app.vault.configDir}/plugins/${this.pluginName}/${fileName}`;
+
+		// https://docs.obsidian.md/Plugins/Releasing/Plugin+guidelines#Use+%60normalizePath()%60+to+clean+up+user-defined+paths
+		return normalizePath(`${basePath}/${relativePath}`);
+	}
+
+	getToken() {
+		const token = process.env.GITHUB_TOKEN || this.settings.githubToken;
+
+		const loadedFrom: "env" | "settings" = process.env.GITHUB_TOKEN
+			? "env"
+			: "settings";
+
+		return token ? { token, loadedFrom } : null;
+	}
+
+	loadEnvVars() {
+		// https://levelup.gitconnected.com/obsidian-plugin-development-tutorial-how-to-use-environment-variables-d6f9258f3957
+		dotenv.config({ path: this.getAbsolutePath(".env") });
+
+		if (!this.getToken()) this.noGithubTokenNotice(true);
+	}
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon(
-			"dice",
-			"Sample Plugin",
-			(evt: MouseEvent) => {
-				// Called when the user clicks the icon.
-				new Notice("This is a notice!");
-			}
-		);
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass("my-plugin-ribbon-class");
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText("Status Bar Text");
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: "open-sample-modal-simple",
-			name: "Open sample modal (simple)",
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: "sample-editor-command",
-			name: "Sample editor command",
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection("Sample Editor Command");
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: "open-sample-modal-complex",
-			name: "Open sample modal (complex)",
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView =
-					this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, "click", (evt: MouseEvent) => {
-			console.log("click", evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(
-			window.setInterval(() => console.log("setInterval"), 5 * 60 * 1000)
-		);
+		this.loadEnvVars();
 	}
 
 	onunload() {}
@@ -106,49 +76,5 @@ export default class MyPlugin extends Plugin {
 
 	async saveSettings() {
 		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const { contentEl } = this;
-		contentEl.setText("Woah!");
-	}
-
-	onClose() {
-		const { contentEl } = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const { containerEl } = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName("Setting #1")
-			.setDesc("It's a secret")
-			.addText((text) =>
-				text
-					.setPlaceholder("Enter your secret")
-					.setValue(this.plugin.settings.mySetting)
-					.onChange(async (value) => {
-						this.plugin.settings.mySetting = value;
-						await this.plugin.saveSettings();
-					})
-			);
 	}
 }
